48675e5fcdad8ac1eb7ce207a7ab001cb4719c6e
Edwin A. Andrade <73005505+Edwin-Lines@users.noreply.github.com>
2024-05-05 12:22:05 -0600

    feat: expansion semantica por lenguaje y tokenizacion (CA-003)
    * feat: expansión semántica por lenguaje y tokenizacion (CA-003)
    
    * fix: uso de principio open-closed para incluir nuevos idiomas (CA-003)
    
    * fix: cambio de librería de reconocimiento de lenguaje

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ app = FastAPI()
 
 
 LANGUAGES_PATH = "json/languages.json"
+
+
 solr_manager = SolrManager()
 
 
@@ -44,10 +46,11 @@ def create_tokens(input):
 def create_synonymsArray(word, language):
     return expander.get_similar_matrix(word, language)
 
-
+  
 def check_core(core_name):
     status_code = solr_manager.get_core_status(core_name)
     if status_code != 200:
         response = solr_manager.create_core(core_name, core_name)
         status_code = response.status_code
     return status_code
+
