3e56c5bf5a37ce38b2514d202b27df4a0d093054
Edwin-Lines <alonsoandrade28@hotmail.com>
2024-05-06 22:29:43 -0600

    feat: cambiando a una arquitectura limpia (CA-008)

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from fastapi import FastAPI
-# import searcher.tokenizer as tokenizer
-# import searcher.expander as expander
-# from infra.solr.solrManager import SolrManager
 from infra.controller.router import api_router
-
-# LANGUAGES_PATH = "json/languages.json"
 
 
 def get_application() -> FastAPI:
@@ -14,11 +9,3 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
-
-# @app.get("/tokens")
-# def create_tokens(input):
-#     return tokenizer.tokenize_input(input, LANGUAGES_PATH)
-
-# @app.get("/synonyms")
-# def create_synonymsArray(word, language):
-#     return expander.get_similar_matrix(word, language)
